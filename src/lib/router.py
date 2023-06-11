b1bbded47e6447bb5a9439ec5de9c8a3bc4713ba
Can Acar <can.acar2@ogr.sakarya.edu.tr>
2023-06-11 03:51:22 +0300

    1.0.3

--- src/lib/router.py
+++ src/lib/router.py
@@ -1,7 +1,10 @@
+import json
+import re
 from typing import List
 
 from flask import abort
 from flask import request
+from flask import Response
 from werkzeug.routing import Map
 
 from src.lib.controller_factory import ControllerFactory
@@ -37,12 +40,35 @@ class Router:
 
         # Go through all routes in the map.
         for route_meta in self.controller_route_map:
-            # Check if the route can handle the request.
-            if route_meta.route == path:
+            # Match the 'api/v1/test/(?P<test_key>[^/]+)-(?P<test_value>[^/]+)' == 'api/v1/test/1-2'
+            if re.match(route_meta.route, path):
                 for entry in self.controller_factory.controllers:
                     if entry.controller.__name__ == route_meta.controller:
-                        controller  = entry.controller()
-                        return getattr(controller, route_meta.action)()
+                        controller = entry.controller()
+
+                        return route_meta.handler(controller)
+            else:
+                header = {'Content-Type': 'application/json'}
+                return Response(json.dumps({'message': 'Not found.', 'status': False}), status=404, headers=header)
 
         # If no route can handle the request, return a 404 error.
-        abort(404)
+        header = {'Content-Type': 'application/json'}
+        return Response(json.dumps({'message': 'Not found.', 'status': False}), status=404, headers=header)
+
+        # , route_meta.action
+        ##return getattr(controller, route_meta.action)()
+        # call the handler function
+        # return route_meta.handler(controller, route_meta.action)
+        # if route_meta.route == path:
+        #     for entry in self.controller_factory.controllers:
+        #         if entry.controller.__name__ == route_meta.controller:
+        #             controller  = entry.controller()
+        #             return getattr(controller, route_meta.action)()
+
+        # If no route can handle the request, return a 404 error.
+        # and decorate
+        # the
+        # response
+        # with the handler's response
+        # return getattr(controller, route_meta.action)()
+        # # return route_meta.handler(getattr(controller, route_meta.action)())
